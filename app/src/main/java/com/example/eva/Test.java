aded8a9555b7c736e703733987de37cf6a31589a
Alexander Jordan <35911038+wurzi200@users.noreply.github.com>
2019-10-22 14:31:51 +0200

    change color if status field #stage^1

--- app/src/main/java/com/example/eva/Test.java
+++ app/src/main/java/com/example/eva/Test.java
@@ -1,8 +1,12 @@
 package com.example.eva;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Test extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class Test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+    }
+
+    public void changeColor(View view) {
+        String test = view.getTag().toString();
+
+        Toast myToast = Toast.makeText(this, test,
+                Toast.LENGTH_SHORT);
+        myToast.show();
+
+        TextView status = findViewById(R.id.textView2);
+
+        int color_id = ContextCompat.getColor(view.getContext(), R.color.colorPrimaryDark);
+        switch(test) {
+            case "yes" :
+                color_id = ContextCompat.getColor(view.getContext(), R.color.yes);
+                break;
+            case "no" :
+                color_id = ContextCompat.getColor(view.getContext(), R.color.no);
+                break;
+            case "maybe" :
+                color_id = ContextCompat.getColor(view.getContext(), R.color.maybe);
+                break;
+        }
+
+        status.setBackgroundColor(color_id);
     }
 }
